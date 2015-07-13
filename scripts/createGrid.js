ea6f559c9a56d2500c0d7e943aa30a4c2d8c94a6
Serge Babayan <serj.b.babayan@gmail.com>
2015-07-12 22:10:08 -0400

    Implemented browserify for better module management

---
+++ scripts/createGrid.js
@@ -0,0 +1,17 @@
+module.exports=function createGrid(canvas,grid_size){
+	//create the harizontal lines of the grid
+  for(var i=0;i<canvas.width;i+=grid_size){
+    canvas.add(new fabric.Line([i,0,i,canvas.height*2],{ 
+      stroke: '#ccc', 
+      selectable: false
+    }));
+  }
+
+  //create the vertical lines of the grid
+  for(var i=0;i<canvas.height;i+=grid_size){
+    canvas.add(new fabric.Line([0,i,canvas.width*2,i],{ 
+      stroke: '#ccc', 
+      selectable: false
+    }));
+  }
+}
--- scripts/dist/truss.js
+++ scripts/dist/truss.js
@@ -1,3 +1,22 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports=function createGrid(canvas,grid_size){
+	//create the harizontal lines of the grid
+  for(var i=0;i<canvas.width;i+=grid_size){
+    canvas.add(new fabric.Line([i,0,i,canvas.height*2],{ 
+      stroke: '#ccc', 
+      selectable: false
+    }));
+  }
+
+  //create the vertical lines of the grid
+  for(var i=0;i<canvas.height;i+=grid_size){
+    canvas.add(new fabric.Line([0,i,canvas.width*2,i],{ 
+      stroke: '#ccc', 
+      selectable: false
+    }));
+  }
+}
+},{}],2:[function(require,module,exports){
   var createGrid=require('./createGrid');
   var grid_size = 15; //pixels per square
 
@@ -81,3 +100,4 @@
   function startSimulation(){
     return false;
   }
+},{"./createGrid":1}]},{},[2]);
